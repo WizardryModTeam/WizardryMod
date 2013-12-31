ca78d3db3902cf3ac9b282dab4b11598f1c53507
KazKing <king.kazzak@gmail.com>
2013-12-31 14:02:33 -0500

    Test

--- src/main/java/wizardymodteam/Main.java
+++ src/main/java/wizardymodteam/Main.java
@@ -1,5 +1,13 @@
 package wizardymodteam;
 
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
 public class Main {
+
+ @EventHandler
+ public void Load(FMLPreInitializationEvent event){
+  System.out.println("whatever");
+  }
 
 }
